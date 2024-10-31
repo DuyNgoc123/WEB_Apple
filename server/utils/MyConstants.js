315bad0d04dbd07837a8dd8f9f69d09fdbefcc63
DuyNgoc123 <128782479+DuyNgoc123@users.noreply.github.com>
2024-10-31 15:16:38 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster1.xxdyqdv.mongodb.net',
+    DB_USER:'luongbo1970',
+    DB_PASS: 'phamgiakhiem447',
+    DB_DATABASE: 'shoppingonline',
+    EMAIL_USER: 'shoppingonlkhim@hotmail.com', //Microsft mail service
+    EMAIL_PASS: 'Phamgiakhiem22022003@',
+    JWT_SECRET: 'HMAC',
+    JWT_EXPIRES: '9999999999999999999999999', // in milliseconds
+};
+module.exports= MyConstants;
